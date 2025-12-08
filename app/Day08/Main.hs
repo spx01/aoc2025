2794fc37ca5fdc66f4761c00da5b08403f1e7bc3
spx01 <42381535+spx01@users.noreply.github.com>
2025-12-09 00:11:18 +0200

    feat: day 8 ST
    Signed-off-by: spx01 <42381535+spx01@users.noreply.github.com>

--- app/Day08/Main.hs
+++ app/Day08/Main.hs
@@ -3,8 +3,9 @@ module Main (main) where
 import qualified AOC
 import Util
 
-import qualified Data.Sequence as S
 import qualified Data.Map as M
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
 import Data.Coerce
 import Control.Arrow
 import Data.Foldable
@@ -13,53 +14,20 @@ import Linear.Metric
 import Data.List.Split
 import Data.List
 import Data.Ord
+import Control.Monad.ST
+import Data.Maybe
 
 data NodeT
 type Node      = TInt NodeT
-type Point     = V3 Double
+type Point     = V3 Int
 type NodeTable = M.Map Point Node
-type DSU       = S.Seq (Node, Int)
-
-mkDSU :: Int -> DSU
-mkDSU n = S.fromList $ zip [0 .. coerce n - 1] (repeat 1)
-
-subSize :: Node -> DSU -> Int
-subSize n d = snd $ d `S.index` coerce n
-
-getParent :: Node -> DSU -> Node
-getParent n d = fst $ d `S.index` coerce n
-
-getRoot :: Node -> DSU -> Node
-getRoot n d = if p == n then n else getRoot p d
-  where
-    p = getParent n d
-
-compSizes :: DSU -> [Int]
-compSizes = filterWithIndex (\i (n, _) -> i == coerce n) >>> fmap snd >>> toList
-
-connect :: Node -> Node -> DSU -> DSU
-connect u v d =
-  if ru == rv
-    then d
-    else
-      if su < sv
-        then aux ru su rv d
-        else aux rv sv ru d
-  where
-    ru = getRoot u d
-    rv = getRoot v d
-    su = subSize ru d
-    sv = subSize rv d
-
-    aux :: Node -> Int -> Node -> DSU -> DSU
-    aux c sc p = id
-      >>> S.adjust' (first (const p)) (coerce c)
-      >>> S.adjust' (second (+ sc)) (coerce p)
+type DSU s     = VM.STVector s (Node, Int)
+type Edge      = (Point, Point)
 
 nodeIx :: Point -> NodeTable -> Node
 nodeIx = flip (M.!)
 
-edgeIx :: (Point, Point) -> NodeTable -> (Node, Node)
+edgeIx :: Edge -> NodeTable -> (Node, Node)
 edgeIx (p1, p2) = nodeIx p1 &&& nodeIx p2
 
 numNodes :: NodeTable -> Int
@@ -68,6 +36,50 @@ numNodes = M.size
 mkTable :: [Point] -> NodeTable
 mkTable ps = M.fromList (zip ps [0 ..])
 
+edges :: [Point] -> [Edge]
+edges ps = sortOn (uncurry qd) [(p1, p2) | p1 <- ps, p2 <- ps, p1 < p2]
+
+mkDSU :: Int -> ST s (DSU s)
+mkDSU n = V.thaw . V.fromList $ [0 .. coerce n - 1] `zip` repeat 1
+
+subSize :: Node -> DSU s -> ST s Int
+subSize n d = snd <$> (d `VM.read` coerce n)
+
+parent :: Node -> DSU s -> ST s Node
+parent n d = fst <$> (d `VM.read` coerce n)
+
+root :: Node -> DSU s -> ST s Node
+root n d = do
+  p <- parent n d
+  if p == n
+    then pure n
+    else root p d
+
+rootData :: DSU s -> ST s [(Node, Int)]
+rootData = VM.ifoldr (\i p@(n, _) a -> if i == coerce n then p : a else a) []
+
+compSizes :: DSU s -> ST s [Int]
+compSizes = fmap (fmap snd) . rootData
+
+connect :: forall s. Node -> Node -> DSU s -> ST s ()
+connect u v d = do
+  ru <- root u d
+  rv <- root v d
+  su <- subSize ru d
+  sv <- subSize rv d
+  case () of
+    _ | ru == rv  -> pure ()
+      | su < sv   -> aux ru su rv
+      | otherwise -> aux rv sv ru
+  where
+    aux :: Node -> Int -> Node -> ST s ()
+    aux c cs p = do
+      VM.modify d (first (const p)) (coerce c)
+      VM.modify d (second (+ cs))   (coerce p)
+
+addEdge :: Edge -> NodeTable -> DSU s -> ST s ()
+addEdge e nt = uncurry connect (edgeIx e nt)
+
 parse :: String -> [Point]
 parse = lines >>> fmap (splitOn ",") >>> fmap toPoint
   where
@@ -75,46 +87,40 @@ parse = lines >>> fmap (splitOn ",") >>> fmap toPoint
     toPoint [x, y, z] = V3 (read x) (read y) (read z)
     toPoint _         = undefined
 
-ordPairs :: (Ord a) => [a] -> [(a, a)]
-ordPairs as = [(a1, a2) | a1 <- as, a2 <- as, a1 < a2]
-
-pointPairs :: [Point] -> [(Point, Point)]
-pointPairs = ordPairs >>> sortOn (uncurry distance)
-
-addEdge :: (Point, Point) -> NodeTable -> DSU -> DSU
-addEdge e nt = uncurry connect (edgeIx e nt)
-
-scanDSUs :: NodeTable -> [(Point, Point)] -> [(DSU, (Point, Point))]
-scanDSUs nt es = scanr (flip addEdge nt) (mkDSU n) res `zip` res
+parse' :: String -> ([Edge], NodeTable)
+parse' s = (es, nt)
   where
-    n   = numNodes nt
-    res = reverse es
-
-solve1 :: [Point] -> [(Point, Point)] -> [Int]
-solve1 ps = fst . head' . scanDSUs nt >>> compSizes
-  where
+    ps = parse s
     nt = mkTable ps
+    es = edges ps
 
-solve2 :: [Point] -> [(Point, Point)] -> (Point, Point)
-solve2 ps = scanDSUs nt >>> span (oneComp . fst) >>> snd . last . fst
+solve1 :: [Edge] -> NodeTable -> [Int]
+solve1 es nt = runST $ do
+  d <- mkDSU (numNodes nt)
+  forM_ (take 1000 es) $ \e -> addEdge e nt d
+  sortOn Down <$> compSizes d
+
+solve2 :: [Edge] -> NodeTable -> Edge
+solve2 es0 nt = fromJust (runST $ mkDSU (numNodes nt) >>= aux es0 Nothing)
   where
-    nt = mkTable ps
-    oneComp :: DSU -> Bool
-    oneComp d = case compSizes d of [_] -> True; _ -> False
+    aux :: [Edge] -> Maybe Edge -> DSU s -> ST s (Maybe Edge)
+    aux [] me _ = pure me
+    aux (e:es) me d = do
+      cs <- compSizes d
+      case cs of
+        [_] -> pure me
+        _   -> do
+          addEdge e nt d
+          aux es (Just e) d
 
 part1 :: String -> String
-part1 = parse
-  >>> (id &&& take 1000 . pointPairs)
-  >>> take 3 . sortOn Down . uncurry solve1
-  >>> product
-  >>> show
+part1 = parse' >>> uncurry solve1 >>> take 3 >>> product >>> show
 
 part2 :: String -> String
-part2 = parse
-  >>> (id &&& pointPairs)
-  >>> uncurry solve2
-  >>> (\((V3 x1 _ _), (V3 x2 _ _)) -> round x1 * round x2 :: Int)
-  >>> show
+part2 = parse' >>> uncurry solve2 >>> extract >>> show
+  where
+    extract :: Edge -> Int
+    extract (V3 x1 _ _, V3 x2 _ _) = x1 * x2
 
 main :: IO ()
 main = AOC.wrapInteract 8 (AOC.combineParts part1 part2)
